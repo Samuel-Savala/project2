df7e050edfcf88cd4c66146cc11ccc42f0cf590d
Samuel Savala <ssavala3@student.rccd.edu>
2026-06-04 01:09:05 -0700

    Initial commit

---
+++ project2.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   project2.cpp
+ * Author: saval
+ *
+ * Created on June 3, 2026, 10:53 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
